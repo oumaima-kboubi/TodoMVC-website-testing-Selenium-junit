71d8de9acd44448356b5a37d9826d146ddb27c96
oumaima-kboubi <oumaima.kboubi@insat.u-carthage.tn>
2021-12-08 01:08:27 +0000

    Exploitation de l'annotation @ParameterizedTest

--- src/main/java/Todomvc.java
+++ src/main/java/Todomvc.java
@@ -16,13 +16,13 @@ public class Todomvc {
     JavascriptExecutor js;
 
     @BeforeAll
-    public static  void init(){
+    public static void init() {
         WebDriverManager.operadriver().setup();
 
     }
 
     @BeforeEach
-    public void initDriver(){
+    public void initDriver() {
         driver = new OperaDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
@@ -35,24 +35,21 @@ public class Todomvc {
         WebElement item = driver.findElement(By.linkText(platform));
         item.click();
     }
+
     private void addTodo(String todo) {
         WebElement item = driver.findElement(By.className("new-todo"));
         item.sendKeys(todo);
         item.sendKeys(Keys.RETURN);
     }
 
-    private void checkTodo() throws InterruptedException{
-        WebElement item1 = driver.findElement(By.cssSelector("li:nth-child(2) .toggle"));
-        item1.click();
-        Thread.sleep(2000);
-        WebElement item2 = driver.findElement(By.cssSelector("li:nth-child(3) .toggle"));
-        item2.click();
-        Thread.sleep(2000);
+    private void checkTodo(int itemNumber) {
+        WebElement item = driver.findElement(By.cssSelector("li:nth-child(" + itemNumber + ") .toggle"));
+        item.click();
     }
 
     private void assertItemLeft(int expectedItemLeftResult) {
         WebElement item = driver.findElement(By.xpath("//footer/*/span | //footer/span"));
-        if(expectedItemLeftResult == 1 ) {
+        if (expectedItemLeftResult == 1) {
             String futureResult = String.format("$d item left", expectedItemLeftResult);
             ExpectedConditions.textToBePresentInElement(item, futureResult);
         } else {
@@ -64,7 +61,7 @@ public class Todomvc {
     @Test
     public void todoCaseTesting() throws InterruptedException {
 
-        //TODO:Naviguer vers le site de todomvc
+        //TODO:Naviguer vers le site de TodoMVC
         driver.get("https://todomvc.com");
         Thread.sleep(2000);
 
@@ -84,18 +81,62 @@ public class Todomvc {
         addTodo("Study SOLID principals");
         Thread.sleep(2000);
 
-        //TODO:Cocher des actions
-        checkTodo();
+        //TODO:Cocher le deuxième todo
+        checkTodo(2);
+        Thread.sleep(2000);
+
+        //TODO:Cocher le troisième todo
+        checkTodo(3);
+        Thread.sleep(2000);
 
         //TODO:Vérification du champs item left
         assertItemLeft(1);
+        Thread.sleep(3000);
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"Backbone.js",
+            "AngularJS",
+            "Dojo",
+            "React"})
+    public void todosCaseTesting(String platform) throws InterruptedException {
+
+        //TODO:Naviguer vers le site de TodoMVC
+        driver.get("https://todomvc.com");
+        Thread.sleep(2000);
+
+        //TODO:Maximiser la fenêtre
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        //TODO:Choisir la platform "Backbone.js"
+        platformChoice(platform);
+        Thread.sleep(2000);
+
+        //TODO:Ajouter 3 todo
+        addTodo("Plan the camp");
+        Thread.sleep(2000);
+        addTodo("Take the dog on a walk");
+        Thread.sleep(2000);
+        addTodo("Study SOLID principals");
+        Thread.sleep(2000);
+
+        //TODO:Cocher le deuxième todo
+        checkTodo(2);
+        Thread.sleep(2000);
+
+        //TODO:Cocher le troisième todo
+        checkTodo(3);
+        Thread.sleep(2000);
+
+        //TODO:Vérification du champs item left
+        assertItemLeft(1);
+        Thread.sleep(2000);
     }
 
 
-
     @AfterEach
-    public void quitDriver() throws InterruptedException {
+    public void quitDriver(){
         driver.quit();
     }
 }
